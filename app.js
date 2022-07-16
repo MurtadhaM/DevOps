0073ad9836069214ca5530a3e092497b3be45422
Murtadha Marzouq <mmarzou0@gmail.com>
2022-07-16 00:42:27 -0400

    👹

--- app.js
+++ app.js
@@ -1,38 +1,63 @@
 const express = require('express');
 
+
+
+
 const app = express();
  
 app.get('/', (req, res) => {
      // index.html is the file we want to serve
-        res.sendFile(__dirname + '/index.html');    
-    }
-
+        res.sendFile(__dirname + '/templates/index.html');
+}
 );
+
 app.get('/Contact', (req, res) => {
     // index.html is the file we want to serve
-       res.sendFile(__dirname + '/contact.html');    
-   }
+    res.sendFile(__dirname + '/templates/contact.html');
+}
 
 );
 
 app.get('/About', (req, res) => {
     // index.html is the file we want to serve
-       res.sendFile(__dirname + '/about.html');    
-   }
+    res.sendFile(__dirname + '/templates/about.html');
+}
 
 );
+
+
+function show_image(image_name) {
+    var img = document.createElement("img");
+    img.src = image_name;
+    document.body.appendChild(img);
+}
+
 
 app.get('/Home', (req, res) => {
     // index.html is the file we want to serve
-       res.sendFile(__dirname + '/home.html');    
-   }
+    console.log(req.query.query);
 
-);
+    
+    res.sendFile(__dirname + '/templates/home.html');
+    
+    
+
+
+    }
+    );
+    
+ 
+
+    
+
+
+
+    
 
 
 
 app.listen(8080, () => {
-        console.log('Example app listening on port 8080!');
+        console.log(' app listening on  http://localhost:8080');
     }
     );
 
